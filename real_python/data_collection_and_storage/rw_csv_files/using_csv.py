9b25f794b18c8adc8a1b04786a28f709944a447f
huntmckay <huntmckay@gmail.com>
2023-02-05 23:13:51 -0500

    simple csv reader stable

---
+++ real_python/data_collection_and_storage/rw_csv_files/using_csv.py
@@ -0,0 +1,27 @@
+# using_csv.py
+
+import csv
+
+with open('employee_birthday.txt') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    line_count = 0
+    for row in csv_reader:
+        if line_count == 0:
+            print(f'Column names are {", ".join(row)}')
+            line_count += 1
+        else:
+            print(f'\t{row[0]} works in the {row[1]} department, and was born in {row[2]}')
+            line_count +=1
+    print(f'Processed {line_count - 1} lines.')
+
+
+"""
+In C (and many other languages), you can insert hard-to-see/type characters using \ notation:
+
+    \a is alert/bell
+    \b is backspace/rubout
+    \n is newline
+    \r is carriage return (return to left margin)
+    \t is tab
+"""
+
