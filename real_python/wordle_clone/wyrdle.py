2d7a9e19594467b07e1d41a829cca37ca69c56a5
huntmckay <huntmckay@gmail.com>
2023-02-28 22:57:18 -0500

    loops and guessing checks

--- real_python/wordle_clone/wyrdle.py
+++ real_python/wordle_clone/wyrdle.py
@@ -1,14 +1,15 @@
 # wyrdle.py
 
-
-guess_count = 0
-
-while guess_count <= 4:
-    guess = input("Guess a word: ").upper()
-    if guess == "SNAKE":
+for guess_count in range(1,7):
+    word = "SNAKE"
+    guess = input(f"\nGuess {guess_count}: ").upper()
+    correct_letters = set(word) & set(guess)
+    #misplaced_letters = 
+    wrong_letters = set(word) - set(guess)
+    print(f"The correct letters are {correct_letters}")
+    print(f"The wrong letters are {wrong_letters}")
+    if guess == word:
         print("correct")
         break
-    else:
-        print("wrong")
-        guess_count = guess_count +1
-    print(guess_count)
+
+    print("wrong")
