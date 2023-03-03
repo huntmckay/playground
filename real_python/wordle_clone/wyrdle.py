2c93b94ae87e11f95a3daf02052a34ee963392ca
huntmckay <huntmckay@gmail.com>
2023-03-02 20:21:09 -0500

    random word selected from list

--- real_python/wordle_clone/wyrdle.py
+++ real_python/wordle_clone/wyrdle.py
@@ -1,20 +1,32 @@
 # wyrdle.py
 
-WORD = "SNAKE"
+import pathlib
+import random
+
+WORDLIST = pathlib.Path("wordlist.txt")
+
+words = [
+    word.upper()
+    for word in WORDLIST.read_text(encoding="utf-8").strip().split("\n")
+]
+word = random.choice(words)
+print(f"Secret word is {word}")
 
 for guess_count in range(1,7):
     guess = input(f"\nGuess {guess_count}: ").upper()
-    if guess == WORD:
+    if guess == word:
         print("correct")
         break
 
 
-    correct_letters = { letter for letter, correct in zip(guess, WORD) if letter == correct }
+    correct_letters = { letter for letter, correct in zip(guess, word) if letter == correct }
 
-    misplaced_letters = set(guess) & set(WORD) - correct_letters
+    misplaced_letters = set(guess) & set(word) - correct_letters
 
-    wrong_letters = set(guess) - set(WORD)
+    wrong_letters = set(guess) - set(word)
 
     print("Correct Letters:", ", ".join(sorted(correct_letters)))
     print("Misplaced Letters:", ", ".join(sorted(misplaced_letters)))
-    print("Wrongn Letters:", ", ".join(sorted(wrong_letters)))
+    print("Wrong Letters:", ", ".join(sorted(wrong_letters)))
+else:
+    print(f"Secret word was {word}")
