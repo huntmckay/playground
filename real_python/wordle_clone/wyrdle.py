49dce03a779956bb859c87427802d8cf8ebc8054
mcstarky <huntmckay@gmail.com>
2023-03-04 14:11:58 -0500

    main process debugged

--- real_python/wordle_clone/wyrdle.py
+++ real_python/wordle_clone/wyrdle.py
@@ -15,36 +15,40 @@ def get_random_word():
     print(f"Secret word is {word}")
     return word
     
-def show_guess():
+def show_guess(guess,word):
 
-    for guess_count in range(1,7):
-        guess = input(f"\nGuess {guess_count}: ").upper()
-        if guess == word:
-            print("correct")
-            break
-    
-    correct_letters = { letter for letter, correct in zip(guess, word) if letter == correct }
-    misplaced_letters = set(guess) & set(word) - correct_letters
-    wrong_letters = set(guess) - set(word)
+    if guess == word:
+        return True
+    else:
+        correct_letters = { letter for letter, correct in zip(guess, word) if letter == correct }
+        misplaced_letters = set(guess) & set(word) - correct_letters
+        wrong_letters = set(guess) - set(word)
 
-    print("Correct Letters:", ", ".join(correct_letters))
-    print("Misplaced Letters:", ", ".join(misplaced_letters))
-    print("Wrong Letters:", ", ".join(wrong_letters))
+        print("Correct Letters:", ", ".join(correct_letters))
+        print("Misplaced Letters:", ", ".join(misplaced_letters))
+        print("Wrong Letters:", ", ".join(wrong_letters))
+        
+def end_game(word,win):
     
-def end_game():
-    
-    print(f"Secret word was {word}")
+    if win:
+        print(f"CORRECT! The Secret word was {word}")
+    else:
+        print(f"LOSER! The Secret word was {word}")
 
-def main()
+def main():
     
     #Pre-Process
-    get_random_word()
+    word = get_random_word()
     
     #Main Process
-    show_guess()
-
+    for guess_count in range(1,7):
+        guess = input(f"\nGuess {guess_count}: ").upper()
+        if show_guess(guess,word):
+            end_game(word,win=True)
+            break
     #Post-Process
-    end_game()
+    else:
+        end_game(word,win=False)
 
 if __name__ == '__main__':
     main()
