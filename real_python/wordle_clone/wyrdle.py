22f04e623cabc8f9aab7a8d3fb34641fe6f2e210
huntmckay <huntmckay@gmail.com>
2023-03-03 22:43:17 -0500

    setup for program layout

--- real_python/wordle_clone/wyrdle.py
+++ real_python/wordle_clone/wyrdle.py
@@ -3,30 +3,48 @@
 import pathlib
 import random
 
-WORDLIST = pathlib.Path("wordlist.txt")
+def get_random_word():
+    
+    WORDLIST = pathlib.Path("wordlist.txt")
 
-words = [
-    word.upper()
-    for word in WORDLIST.read_text(encoding="utf-8").strip().split("\n")
-]
-word = random.choice(words)
-print(f"Secret word is {word}")
+    words = [
+        word.upper()
+        for word in WORDLIST.read_text(encoding="utf-8").strip().split("\n")
+    ]
+    word = random.choice(words)
+    print(f"Secret word is {word}")
+    return word
+    
+def show_guess():
 
-for guess_count in range(1,7):
-    guess = input(f"\nGuess {guess_count}: ").upper()
-    if guess == word:
-        print("correct")
-        break
-
-
+    for guess_count in range(1,7):
+        guess = input(f"\nGuess {guess_count}: ").upper()
+        if guess == word:
+            print("correct")
+            break
+    
     correct_letters = { letter for letter, correct in zip(guess, word) if letter == correct }
-
     misplaced_letters = set(guess) & set(word) - correct_letters
-
     wrong_letters = set(guess) - set(word)
 
     print("Correct Letters:", ", ".join(correct_letters))
     print("Misplaced Letters:", ", ".join(misplaced_letters))
     print("Wrong Letters:", ", ".join(wrong_letters))
-else:
+    
+def end_game():
+    
     print(f"Secret word was {word}")
+
+def main()
+    
+    #Pre-Process
+    get_random_word()
+    
+    #Main Process
+    show_guess()
+
+    #Post-Process
+    end_game()
+
+if __name__ == '__main__':
+    main()
